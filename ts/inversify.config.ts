60603ca685eda8fe40adf09536ae090827e4c6e7
John Reeme <john.reeme@soteradefense.com>
2016-11-07 09:01:49 -0600

    -> v.0.2.4

--- ts/inversify.config.ts
+++ ts/inversify.config.ts
@@ -7,11 +7,14 @@ import {DockerUtil} from "./interfaces/docker-util";
 import {DockerUtilImpl} from "./implementations/docker-util-impl";
 import {DockerContainerManagement} from "./interfaces/docker-container-management";
 import {DockerContainerManagementImpl} from "./implementations/docker-container-management-impl";
-import {kernel as yargsKernel, CommandUtil, Command, Spawn, CommandLine} from 'firmament-yargs';
+import {kernel as yargsKernel, CommandUtil, Command, Spawn, CommandLine, ProgressBar} from 'firmament-yargs';
 import {FirmamentDocker} from "./interfaces/firmament-docker";
 import {FirmamentDockerImpl} from "./implementations/firmament-docker-impl";
 import {DockerCommandImpl} from "./implementations/docker-command-impl";
+import {NestedYargs} from "firmament-yargs/js/interfaces/nested-yargs-wrapper";
 var commandUtil = yargsKernel.get<CommandUtil>('CommandUtil');
+var nestedYargs = yargsKernel.get<NestedYargs>('NestedYargs');
+var progressBar = yargsKernel.get<ProgressBar>('ProgressBar');
 var commandLine = yargsKernel.get<CommandLine>('CommandLine');
 var command = yargsKernel.get<Command>('Command');
 var spawn = yargsKernel.get<Spawn>('Spawn');
@@ -20,6 +23,8 @@ var kernel = new Kernel();
 //TODO: Begin Super HACK -> v.3 of inversify will provide kernel merging but for 2.x we need
 //TODO: a gruesome hack so our constructor injection will work with external types
 kernel.bind<CommandUtil>('CommandUtil').to(<new (args:any)=>CommandUtil>commandUtil.constructor);
+kernel.bind<NestedYargs>('NestedYargs').to(<new (args:any)=>NestedYargs>nestedYargs.constructor);
+kernel.bind<ProgressBar>('ProgressBar').to(<new (args:any)=>ProgressBar>progressBar.constructor);
 kernel.bind<CommandLine>('CommandLine').to(<new (args:any)=>CommandLine>commandLine.constructor);
 kernel.bind<Command>('Command').to(<new (args:any)=>Command>command.constructor);
 kernel.bind<Spawn>('Spawn').to(<new (args:any)=>Spawn>spawn.constructor);
