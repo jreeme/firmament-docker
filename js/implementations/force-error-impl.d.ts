293e1863324dea18c576565061780ee7f50d91f5
John Reeme <john.reeme@soteradefense.com>
2016-11-13 19:29:19 -0600

    -> v.0.2.21 (refactoring a bit, improving low level tests)

--- js/implementations/force-error-impl.d.ts
+++
@@ -1,6 +0,0 @@
-/// <reference types="node" />
-import { ForceError } from "../interfaces/force-error";
-export declare class ForceErrorImpl implements ForceError {
-    forceError: boolean;
-    checkForceError(cb: (err: Error, res: any) => void, message?: string): boolean;
-}
