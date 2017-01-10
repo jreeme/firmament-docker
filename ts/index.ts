39d42d5e6c92011413402708a07a39b36c2a913c
John Reeme <john.reeme@soteradefense.com>
2017-01-10 10:05:52 -0600

    v.1.2.0->NPM (new yargs)

--- ts/index.ts
+++ ts/index.ts
@@ -2,3 +2,9 @@ export * from './interfaces/dockerode';
 export * from './interfaces/docker-descriptors';
 import kernel from "./inversify.config";
 export {kernel};
+
+//HACK so tools can get version to sync the firmament ecosystem
+if(process.argv[2] && process.argv[2].toString() === '--version'){
+  let package_json = require('../package.json');
+  console.log(`${package_json.name}: ${package_json.version}`);
+}
