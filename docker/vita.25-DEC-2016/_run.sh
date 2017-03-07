930f0ffea462528af55592674e0a5bb1defade37
John Reeme <john.reeme@soteradefense.com>
2017-03-07 14:48:56 -0600

    v.1.3.7->NPM Patched the edge case of an Image having <null> RepoTags value. While I was there also patched Container having <null> Names property.

--- docker/vita.25-DEC-2016/_run.sh
+++ docker/vita.25-DEC-2016/_run.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-docker run -dt --name vita-strongloop -h vita-strongloop -p 8701:8701 -p 8080:8080 -p 8888:8888 jreeme/vita:25-DEC-2016
+docker run -dt --name vita-strongloop -h vita-strongloop -p 27017:27017 -p 8701:8701 -p 8080:8080 -p 8888:8888 jreeme/vita:25-DEC-2016
 
