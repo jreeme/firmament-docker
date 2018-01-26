20527cef3c7616fb9181e0016376251d8c9e764a
John <john.reeme@gmail.com>
2018-01-26 13:39:10 -0600

    Get to running elasticsearch-head

--- docker/nginx/1.13.8/_run.sh
+++ docker/nginx/1.13.8/_run.sh
@@ -1,3 +1,5 @@
 #!/bin/bash
-docker run -dt --name nginx -h nginx -p 80:80 52.0.211.45:5000/nginx:1.13.8
+docker run -dt --name nginx -h nginx -p 5000:5000 -p 80:80 \
+--mount type=bind,source=/mnt/RAM_disk/firmament-docker/docker/nginx/docker-mount,target=/etc/nginx/conf.d \
+52.0.211.45:5000/nginx:1.13.8
 
