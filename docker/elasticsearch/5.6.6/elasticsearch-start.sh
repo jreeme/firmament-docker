d7cfbbc92da6d4f33f5694d63037cf76b140b84f
John <john.reeme@gmail.com>
2018-02-16 14:55:09 -0600

    Remove su-exec. Use supervisord's user to lower priv for elasticsearch (which won't run as root)

--- docker/elasticsearch/5.6.6/elasticsearch-start.sh
+++ docker/elasticsearch/5.6.6/elasticsearch-start.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 echo "Trying to start elasticsearch @ "`date`
 
-su-exec elasticsearch elasticsearch
+elasticsearch
 
 sleep 5
 echo "elasticsearch bailed out. Attempting restart @ "`date`
