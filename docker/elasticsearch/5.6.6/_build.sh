b34d9f45acf8cdc7a68f483673052a1e6ba4dc3e
John <john.reeme@gmail.com>
2018-02-16 15:01:57 -0600

    Correct openstack ip address

--- docker/elasticsearch/5.6.6/_build.sh
+++ docker/elasticsearch/5.6.6/_build.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-docker build --rm -t 52.0.211.45:5000/elasticsearch:5.6.6 -t 10.1.170.193:5000/elasticsearch:5.6.6 .
+docker build --rm -t 52.0.211.45:5000/elasticsearch:5.6.6 -t 10.1.70.193:5000/elasticsearch:5.6.6 .
 
